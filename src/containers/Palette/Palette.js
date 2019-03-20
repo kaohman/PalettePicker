f5ec207323eb7cbe5ae808788b134f4f10c48df3
Karin Ohman <kaohma@gmail.com>
2019-03-19 21:15:01 -0600

    Add random color functionality

--- src/containers/Palette/Palette.js
+++ src/containers/Palette/Palette.js
@@ -1,37 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-export const Palette = ({ currentPalette }) => {
-  return(
-    <div>
-      <div className='palette-div'>
-        {/* {
-          Object.keys(currentPalette).map(item => <div className={`color-div ${currentPalette[item].color}`}></div>)
-        } */}
-        <div className='color-div'>
-          <button className='unlock-button'></button>
-          <h4>#FFFFFF</h4>
+export class Palette extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      colors: []
+    }
+  }
+  
+  setRandomColors = (id) => {
+    let colors = [];
+    for (let i = 0; i < 5; i++) {
+      const color = `#${Math.random().toString(16).slice(2, 8)}`;
+      colors.push(color);
+    }
+    this.setState({
+      colors
+    })
+  }
+
+  componentDidMount = () => {
+    this.setRandomColors();
+  }
+
+  render() {
+    const { colors } = this.state;
+    return(
+      <div>
+        <div className='palette-div'>
+          {
+            colors.map(color => {
+              return(
+                <div style={{ backgroundColor: color }} className='color-div'>
+                  <button className='unlock-button'></button>
+                  <h4>{color}</h4>
+                </div>
+              )
+            })
+          }
         </div>
-        <div className='color-div'>
-          <button className='unlock-button'></button>
-          <h4>#FFFFFF</h4>
-        </div>
-        <div className='color-div'>
-          <button className='unlock-button'></button>
-          <h4>#FFFFFF</h4>
-        </div>
-        <div className='color-div'>
-          <button className='unlock-button'></button>
-          <h4>#FFFFFF</h4>
-        </div>
-        <div className='color-div'>
-          <button className='unlock-button'></button>
-          <h4>#FFFFFF</h4>
-        </div>
+        <button className='generate-button standard-button'>Generate Palette</button>
       </div>
-      <button className='generate-button standard-button'>Generate Palette</button>
-    </div>
-  )
+    )
+  }
 }
 
 const mapStateToProps = (state) => ({
