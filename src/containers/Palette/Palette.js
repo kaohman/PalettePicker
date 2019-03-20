d765c7a322b4a3085c309fdd1005d457729c6877
Karin Ohman <kaohma@gmail.com>
2019-03-19 21:38:26 -0600

    Add lock, unlock functionaloity

--- src/containers/Palette/Palette.js
+++ src/containers/Palette/Palette.js
@@ -5,19 +5,28 @@ export class Palette extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      colors: []
+      colors: [],
+      locked: []
     }
   }
   
-  setRandomColors = (id) => {
-    let colors = [];
+  setRandomColors = () => {
+    const { colors, locked } = this.state;
+    let newColors = [];
     for (let i = 0; i < 5; i++) {
-      const color = `#${Math.random().toString(16).slice(2, 8)}`;
-      colors.push(color);
+      const color = locked.includes(i.toString()) ? colors[i] : `#${Math.random().toString(16).slice(2, 8)}`;
+      newColors.push(color.toUpperCase());
     }
     this.setState({
-      colors
+      colors: newColors
     })
+  }
+
+  toggleLock = (e) => {
+    const { locked } = this.state;
+    const id = e.target.id;
+    const newLocked = locked.includes(id) ? locked.filter(lockedId => lockedId !== id) : [...locked, id];
+    this.setState({ locked: newLocked });
   }
 
   componentDidMount = () => {
@@ -25,22 +34,27 @@ export class Palette extends Component {
   }
 
   render() {
-    const { colors } = this.state;
+    const { colors, locked } = this.state;
     return(
       <div>
         <div className='palette-div'>
           {
-            colors.map(color => {
+            colors.map((color, i) => {
               return(
                 <div style={{ backgroundColor: color }} className='color-div'>
-                  <button className='unlock-button'></button>
+                  <button 
+                    onClick={this.toggleLock}
+                    className={locked.includes(i.toString()) ? 'lock lock-button' : 'unlock lock-button'}
+                    id={i}
+                  >
+                  </button>
                   <h4>{color}</h4>
                 </div>
               )
             })
           }
         </div>
-        <button className='generate-button standard-button'>Generate Palette</button>
+        <button onClick={this.setRandomColors} className='generate-button standard-button'>Generate Palette</button>
       </div>
     )
   }
