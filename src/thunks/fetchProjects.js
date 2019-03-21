f7f2e8b8dac0c91ba7e381cd19c9f581014d1a54
Matthew Foxwell <foxwellmw@gmail.com>
2019-03-21 17:06:19 -0600

    Add fetchProjects thunk

--- src/thunks/fetchProjects.js
+++ src/thunks/fetchProjects.js
@@ -1,1 +1,15 @@
 import { setLoading, setError, setProjects } from '../actions';
+import { fetchData } from '../utils/api';
+
+export const fetchProjects = () => {
+  return async (dispatch) => {
+    try {
+      dispatch(setLoading(true));
+      const result = await fetchData('/api/v1/projects', 'GET');
+      dispatch(setProjects(result));
+      dispatch(setLoading(false));
+    } catch (error) {
+      dispatch(setError(error.message));
+    }
+  }
+}
