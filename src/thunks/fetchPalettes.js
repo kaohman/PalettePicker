39d260ebff0db43164001cb305ce259c10290132
Karin Ohman <kaohma@gmail.com>
2019-03-25 15:57:54 -0600

    Add functionality to sort projects and palettes based on updated_at property directly in ProjectContainer component

--- src/thunks/fetchPalettes.js
+++ src/thunks/fetchPalettes.js
@@ -5,13 +5,7 @@ export const fetchPalettes = (allProjects) => {
   return (dispatch) => {
     const promisedPalettes = allProjects.map(async palette => {
       try {
-        let allPalettes = await fetchData(`/api/v1/projects/${palette.id}/palettes`, 'GET');
-        allPalettes.sort((a, b) => {
-          if (a.updated_at > b.updated_at) return -1;
-          if (b.updated_at < a.updated_at) return 1;
-          return 0;
-        });
-        return allPalettes
+        return await fetchData(`/api/v1/projects/${palette.id}/palettes`, 'GET');
       } catch (error) {
         dispatch(setError(error.message));
       }
