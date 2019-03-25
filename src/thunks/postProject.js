10c2c3d5c7b23fca10ffac176c93dc1cd049fc2a
Karin Ohman <kaohma@gmail.com>
2019-03-25 16:12:25 -0600

    Update setLoading dispatch

--- src/thunks/postProject.js
+++ src/thunks/postProject.js
@@ -7,9 +7,11 @@ export const postProject = (name) => {
       dispatch(setLoading(true));
       const newProject = await fetchData('/api/v1/projects', 'POST', { name });
       dispatch(addProject(newProject[0]));
+      dispatch(setLoading(false));
       return newProject[0].id
     } catch (error) {
       dispatch(setError(error.message));
+      dispatch(setLoading(false))
     }
   }
 }
