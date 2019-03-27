9246a5f74bdee5d182744b95ca96d0badd9d5585
Matthew Foxwell <foxwellmw@gmail.com>
2019-03-27 12:44:52 -0600

    Add App tests and snapshots

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,17 +1,71 @@
 import React from 'react';
-import App from './App';
-import { shallow } from 'enzyme';
+import { App } from './App';
+import { Home } from '../Home/Home';
+import { Share } from '../Share/Share';
+import { NotFound } from '../../components/NotFound/NotFound';
+import { shallow, mount } from 'enzyme'
+import { MemoryRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from '../../reducers/index';
+import thunk from 'redux-thunk';
 
 describe('App', () => {
+  const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
   let wrapper;
 
-  beforeEach(() => {
-    wrapper = shallow(
-      <App />
-    )
-  });
-
   it('should match the correct snapshot', () => {
+    wrapper = shallow(<App />);
     expect(wrapper).toMatchSnapshot();
   });
+
+  describe('Routes', () => {
+
+    it('should render the Home container when at the root route', () => {
+      wrapper = mount(
+        <Provider store={store}>
+          <MemoryRouter initialEntries={['/']}>
+            <App />
+          </MemoryRouter>
+        </Provider>
+      );
+      expect(wrapper.find(Home)).toHaveLength(1);
+    });
+
+    it('should render the Share container when at route /project/:id', () => {
+      wrapper = mount(
+        <Provider store={store}>
+          <MemoryRouter initialEntries={['/project/1']}>
+            <App />
+          </MemoryRouter>
+        </Provider>
+      );
+      expect(wrapper.find(Share)).toHaveLength(1);
+    });
+
+    it('should render the Share container when at route /project/:id', () => {
+      wrapper = mount(
+        <Provider store={store}>
+          <MemoryRouter initialEntries={['/palette/1']}>
+            <App />
+          </MemoryRouter>
+        </Provider>
+      );
+      expect(wrapper.find(Share)).toHaveLength(1);
+    });
+
+    it('should render the NotFound container when at a route that is not recognized', () => {
+      const wrapper = mount(
+        <Provider store={store}>
+          <MemoryRouter initialEntries={['/banana']}>
+            <App />
+          </MemoryRouter>
+        </Provider>
+      );
+      expect(wrapper.find(NotFound)).toHaveLength(1);
+    });
+
+  });
+
 });
